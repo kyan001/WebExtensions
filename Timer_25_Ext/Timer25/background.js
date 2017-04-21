33e398fcdf8f1e8a76a27200c2c77fb3b0bb548d
Kyan <ykyankai@gmail.com>
2017-04-21 21:43:17 +0800

    read option to pop up a notification or an alert.

--- Timer_25_Ext/Timer25/background.js
+++ Timer_25_Ext/Timer25/background.js
@@ -3,46 +3,47 @@ var current
 var count_timer
 var alert_timer
 var running = false
+var messages = [
+    'Enjoy your time.',
+    "Yesterday's the past, tomorrow's the future, but today is a gift. That's why it's called the present.",
+    "Better three hours too soon than a minute too late.",
+    "No matter how busy you are, you must take time to make the other person feel important.",
+    "Lost time is never found again.",
+    "Time flies over us, but leaves its shadow behind.",
+    "Time is what we want most, but what we use worst.",
+    "Patience and time do more than strength or passion.",
+    "We must use time wisely and forever realize that the time is always ripe to do right.",
+    "If you spend too much time thinking about a thing, you'll never get it done.",
+    "The time you enjoy wasting is not wasted time.",
+    "Time is the most valuable thing a man can spend.",
+    "Time and tide wait for no man.",
+    "If you don't have time to do it right, when will you have time to do it over?",
+]
 
 function reset(){
     current = 25
     clearTimeout(count_timer);
-    clearTimeout(alert_timer);
     chrome.browserAction.setBadgeText({text: ""});
 }
 
 function updateTimerIcon(){
     if (current == 0) {
         stop()
-        popNotification()
+        popMessageBox()
         return
     }
-    count_timer = setTimeout("updateTimerIcon()", 60000);  // per 60 sec
+    count_timer = setTimeout("updateTimerIcon()", 60);  // per 60 sec
     chrome.browserAction.setBadgeText({text: current.toString()+"m"});
     current--;
 }
 
 function popAlert(){
-    alert("TIME'S UP!\n\n" + (new Date()).toLocaleTimeString())
+    message_index = Math.round(Math.random()*(messages.length-1))
+    message = '"' + messages[message_index] + '"'
+    alert("TIME'S UP!" + '\n\n' + message + '\n\n' + (new Date()).toLocaleTimeString())
 }
 
 function popNotification(){
-    messages = [
-        'Enjoy your time.',
-        "Yesterday's the past, tomorrow's the future, but today is a gift. That's why it's called the present.",
-        "Better three hours too soon than a minute too late.",
-        "No matter how busy you are, you must take time to make the other person feel important.",
-        "Lost time is never found again.",
-        "Time flies over us, but leaves its shadow behind.",
-        "Time is what we want most, but what we use worst.",
-        "Patience and time do more than strength or passion.",
-        "We must use time wisely and forever realize that the time is always ripe to do right.",
-        "If you spend too much time thinking about a thing, you'll never get it done.",
-        "The time you enjoy wasting is not wasted time.",
-        "Time is the most valuable thing a man can spend.",
-        "Time and tide wait for no man.",
-        "If you don't have time to do it right, when will you have time to do it over?",
-    ]
     message_index = Math.round(Math.random()*(messages.length-1))
     message = '"' + messages[message_index] + '"'
     chrome.notifications.create({
@@ -55,7 +56,21 @@ function popNotification(){
         isClickable: true,  // show hand pointer when hover
         requireInteraction: true,  // do not close until click
     })
-    alert_timer = setTimeout('popAlert()', 60000);  // 1 min
+}
+
+function popMessageBox(){
+    chrome.storage.sync.get({msgStyle: 'notif'}, function(items){
+        switch (items.msgStyle) {
+            case 'notif':
+                popNotification()
+                break;
+            case 'alert':
+                popAlert()
+                break;
+            default:
+                alert('Message Style Option ' + ites.msgStyle + ' is not valid')
+        }
+    });
 }
 
 function start(){
@@ -74,7 +89,7 @@ function clickToggle() {
         // stop() -> reset()
         stop()
     } else {
-        // start() -> updateTimerIcon()*25 -> popNotification() -> popAlert()
+        // start() -> updateTimerIcon()*25 -> popMessageBox() -> popAlert()/popNotification()
         start()
     }
 }
@@ -83,6 +98,5 @@ chrome.browserAction.onClicked.addListener(clickToggle);
 chrome.browserAction.setIcon({path: "Timer.png"});
 chrome.browserAction.setBadgeText({text: ""});
 chrome.notifications.onClicked.addListener(function(notificationid){
-    clearTimeout(alert_timer)
     chrome.notifications.clear(notificationid)
 })
