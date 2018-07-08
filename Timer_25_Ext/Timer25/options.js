9cb75120092ce83d0596634c6a20b6343db49a78
Kyan <ykyankai@gmail.com>
2018-07-08 22:00:13 +0800

    click img also select radio

--- Timer_25_Ext/Timer25/options.js
+++ Timer_25_Ext/Timer25/options.js
@@ -1,37 +1,50 @@
 function load_options(){
     chrome.storage.sync.get({msgStyle: "notif"}, function(items) {
-        var msg_style_ele = document.getElementsByName("msg-style")
-        for(var i=0; i < msg_style_ele.length; i++){
-            if(msg_style_ele[i].value == items.msgStyle){
-                msg_style_ele[i].checked = true
+        var msgstyle_input_list = document.querySelectorAll("input[name='msgstyle'")
+        for(var i=0; i < msgstyle_input_list.length; i++){
+            var current_ele = msgstyle_input_list[i]
+            if (current_ele.value == items.msgStyle){
+                current_ele.checked = true
             }
         }
     })
 }
 
+var stts_clr_tmr
 function save_options(){
-    var msg_style_ele = document.getElementsByName("msg-style")
-    var msg_style = "notif"  // default
-    for(var i=0; i < msg_style_ele.length; i++){
-        if(msg_style_ele[i].checked){
-            msg_style = msg_style_ele[i].value
+    var msgstyle_input_list = document.querySelectorAll("input[name='msgstyle'")
+    var msgstyle = "notif"  // default
+    for(var i=0; i < msgstyle_input_list.length; i++){
+        var current_ele = msgstyle_input_list[i]
+        if (current_ele.checked){
+            msgstyle = current_ele.value
         }
     }
-    chrome.storage.sync.set({msgStyle: msg_style}, function(){
-        var status = document.getElementById("status-" + msg_style)
-        status.textContent = chrome.i18n.getMessage("saved")
-        setTimeout(function() {
-            status.textContent = ""
+    chrome.storage.sync.set({msgStyle: msgstyle}, function(){
+        var status = document.querySelector("#status")
+        status.style.display = "inline"
+        clearTimeout(stts_clr_tmr)
+        stts_clr_tmr = setTimeout(function() {
+            status.style.display = "none"
         }, 1000)
     })
 }
 
 function i18n(){
-    document.getElementById("msg-box-style-label").innerHTML = chrome.i18n.getMessage("messageBoxStyle")
+    document.getElementById("msgstyle__label").innerHTML = chrome.i18n.getMessage("messageBoxStyle")
+    document.querySelector("#status").textContent = chrome.i18n.getMessage("saved")
 }
 
+// document.ready events
 document.addEventListener("DOMContentLoaded", load_options)
 document.addEventListener("DOMContentLoaded", i18n)
-document.getElementsByName("msg-style").forEach(function(val, ind, arr){
+
+// click events
+document.querySelectorAll("input[name='msgstyle']").forEach(function(val, ind, arr){
     arr[ind].addEventListener("click", save_options)
+})
+document.querySelectorAll("img.as-radio").forEach(function(val, ind, arr){
+    arr[ind].addEventListener("click", function(){
+        this.parentNode.parentNode.querySelector("input[name='msgstyle']").click()
+    })
 })
